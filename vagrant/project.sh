4ecaa01fbc5715afbcd50f78932dd7dd836a3b07
Niclas Pahlfer <niclas@pahlfer.com>
2015-04-02 13:13:53 +0200

    Rearranged the execution order and some bugfixes

--- vagrant/project.sh
+++ vagrant/project.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Checks if linux or OSX machine and sets the "sed" command accordingly
+darwin=false;
+case "`uname`" in
+  Darwin*) darwin=true ;;
+esac
+
+if $darwin; then
+  sedi="/usr/bin/sed -i ''"
+else
+  sedi="sed -i"
+fi
+
 while true; do
 	read -p "Enter project name: " pNAME
 	if [ -z $pNAME ]; then
@@ -19,11 +31,7 @@ while true; do
 	fi
 done
 
-git clone https://github.com/antoniozzo/vagrant-template.git $pDIR/tmp
-rm -rf $pDIR/tmp/.git
-mv $pDIR/tmp/* $pDIR/
-rm -rf $pDIR/tmp
-cd $pDIR
+IP=192.168.80.$(find ../ -maxdepth 1 -type d -print | wc -l | sed -e 's/^[ \t]*//')
 
 read -p "Do you wish to configure the VM yourself? [Default: no]: " pCONF
 if [[ $pCONF =~ ^[Yy] ]]; then
@@ -33,7 +41,7 @@ if [[ $pCONF =~ ^[Yy] ]]; then
 	read -p "Enter memory size [Default: 1024]: " pMEM
 	read -p "Enter private network IP (Required for vhosting) [Default: ${IP}]: " pIP
 	if [ ! -z $pIP ]; then
-		read -p "Do you wish use a vhost? (Requires sudo) [Default: ${pNAME}.dev]: " pVHOST
+		read -p "Do you wish use a vhost? (Requires sudo) [Default: ${pNAME}\.dev]: " pVHOST
 	fi
 	read -p "Do you wish to install GIT on the VM? [Default: yes]: " pGIT
 	read -p "Do you wish to install NODE on the VM? [Default: yes]: " pNODE
@@ -79,6 +87,7 @@ else
 	unset pSSHPORT
 fi
 
+
 pBOX=${pBOX:-ubuntu/trusty64}
 pTDIR=${pTDIR:-/var/www/}
 pMEM=${pMEM:-1024}
@@ -95,21 +104,26 @@ pVAGRANT=${pVAGRANT:-yes}
 URL=https://vagrantcloud.com/ubuntu/trusty64
 pBOXURL=${pBOXURL:-$URL}
 
-IP=192.168.80.$(find ../ -maxdepth 1 -type d -print | wc -l | sed -e 's/^[ \t]*//')
 pIP=${pIP:-$IP}
+
+git clone https://github.com/antoniozzo/vagrant-template.git $pDIR/tmp
+rm -rf $pDIR/tmp/.git
+mv $pDIR/tmp/* $pDIR/
+rm -rf $pDIR/tmp
+cd $pDIR
 
 DIR=$pTDIRpublic
 pPUBDIR=${pPUBDIR:-$DIR}
 
-sed -i '' "s|pNAME|${pNAME}|g" Vagrantfile
-sed -i '' "s|pBOXNAME|${pBOX}|g" Vagrantfile
-sed -i '' "s|pBOXURL|${pBOXURL}|g" Vagrantfile
-sed -i '' "s|pTDIR|${pTDIR}|g" Vagrantfile
-sed -i '' "s|pMEM|${pMEM}|g" Vagrantfile
+$sedi "s|pNAME|${pNAME}|g" Vagrantfile
+$sedi "s|pBOXNAME|${pBOX}|g" Vagrantfile
+$sedi "s|pBOXURL|${pBOXURL}|g" Vagrantfile
+$sedi "s|pTDIR|${pTDIR}|g" Vagrantfile
+$sedi "s|pMEM|${pMEM}|g" Vagrantfile
 
 if [ ! -z $pIP ]; then
-	sed -i '' "s|#pIP ||g" Vagrantfile
-	sed -i '' "s|pIP|${pIP}|g" Vagrantfile
+	$sedi "s|#pIP ||g" Vagrantfile
+	$sedi "s|pIP|${pIP}|g" Vagrantfile
 
 	if [ ! -z $pVHOST ]; then
 		sudo bash -c "echo -e '${pIP}\t${pVHOST}' >> /etc/hosts"
@@ -117,49 +131,49 @@ if [ ! -z $pIP ]; then
 fi
 
 if [[ $pGIT =~ ^[Yy] ]]; then
-	sed -i '' "s|#pGIT ||g" Vagrantfile
+	$sedi "s|#pGIT ||g" Vagrantfile
 fi
 
 if [[ $pNODE =~ ^[Yy] ]]; then
-	sed -i '' "s|#pNODE ||g" Vagrantfile
+	$sedi "s|#pNODE ||g" Vagrantfile
 fi
 
 if [[ $pPHP =~ ^[Yy] ]]; then
-	sed -i '' "s|#pPHP ||g" Vagrantfile
-	sed -i '' "s|pPUBDIR|${pPUBDIR}|g" provision/php.sh
+	$sedi "s|#pPHP ||g" Vagrantfile
+	$sedi "s|pPUBDIR|${pPUBDIR}|g" provision/php.sh
 
 	if [[ $pCOMPOSER =~ ^[Yy] ]]; then
-		sed -i '' "s|#pCOMPOSER ||g" Vagrantfile
+		$sedi "s|#pCOMPOSER ||g" Vagrantfile
 	fi
 fi
 
 if [[ $pMYSQL =~ ^[Yy] ]]; then
-	sed -i '' "s|#pMYSQL ||g" Vagrantfile
-	sed -i '' "s|pDBNAME|${pDBNAME}|g" provision/mysql.sh
-	sed -i '' "s|pDBPASS|${pDBPASS}|g" provision/mysql.sh
+	$sedi "s|#pMYSQL ||g" Vagrantfile
+	$sedi "s|pDBNAME|${pDBNAME}|g" provision/mysql.sh
+	$sedi "s|pDBPASS|${pDBPASS}|g" provision/mysql.sh
 
 	if [[ $pPHP =~ ^[Yy] ]]; then
-		sed -i '' "s|#pPHPMYSQL ||g" provision/mysql.sh
+		$sedi "s|#pPHPMYSQL ||g" provision/mysql.sh
 	fi
 fi
 
 if [ ! -z $pAPACHEPORT ]; then
-	sed -i '' "s|#pAPACHEPORT ||g" Vagrantfile
-	sed -i '' "s|pAPACHEPORT|${pAPACHEPORT}|g" Vagrantfile
+	$sedi "s|#pAPACHEPORT ||g" Vagrantfile
+	$sedi "s|pAPACHEPORT|${pAPACHEPORT}|g" Vagrantfile
 fi
 
 if [ ! -z $pMYSQLPORT ]; then
-	sed -i '' "s|#pMYSQLPORT ||g" Vagrantfile
-	sed -i '' "s|pMYSQLPORT|${pMYSQLPORT}|g" Vagrantfile
+	$sedi "s|#pMYSQLPORT ||g" Vagrantfile
+	$sedi "s|pMYSQLPORT|${pMYSQLPORT}|g" Vagrantfile
 fi
 
 if [ ! -z $pSSHPORT ]; then
-	sed -i '' "s|#pSSHPORT ||g" Vagrantfile
-	sed -i '' "s|pSSHPORT|${pSSHPORT}|g" Vagrantfile
+	$sedi "s|#pSSHPORT ||g" Vagrantfile
+	$sedi "s|pSSHPORT|${pSSHPORT}|g" Vagrantfile
 fi
 
 if [[ $pVAGRANT =~ ^[Yy] ]]; then
 	vagrant up
 fi
 
-echo "\n\nYour project is running in http://${pVHOST}\n\n"
+echo -e "\n\nYour project is running in http://${pVHOST}\n\n"
